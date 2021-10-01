2320676f5e92d8772d7afe38108121bd3219b73b
kelcheone <kelcheone@gmail.com>
2021-10-01 18:18:35 +0300

    testing transactions

--- test/Token.js
+++ test/Token.js
@@ -2,6 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Token contract", function () {
+  // test 1
+
   it("Deploymwnt should assingn the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
 
@@ -11,5 +13,23 @@ describe("Token contract", function () {
 
     const ownerBalnce = await hardhatToken.balanceOf(owner.address);
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalnce);
+  });
+
+  // tests 2
+
+  it("Should transfer tokens between accounts", async function () {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("Token");
+
+    const hardhatToken = await Token.deploy();
+
+    //Transfer 50 tokens from owner to addr1
+    await hardhatToken.transfer(addr1.address, 50);
+    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+
+    // transfer 50 tokens from addr1 to addr2
+    await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
   });
 });
